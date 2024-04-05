9b8aa357da9cb822e01304a5c730304ab1461ac7
Jacardenas27 <andrescardenas287t@gmail.com>
2024-04-05 11:01:43 -0500

    Módulo para agregar servicios

--- servicios/servicios.js
+++ servicios/servicios.js
@@ -4,49 +4,55 @@ let servicios = [
         "id": 1,
         "nombre": "DataManagement",
         "descripcion": "Aplicación con proposito de análisis de datos y graficación de datos.",
-        "precio": 110000
+        "precio": 110000,
+        "imagen": "dashboard.png"
     },
     {
         "id": 2,
         "nombre": "MarketSys",
         "descripcion": "Aplicación para gestión de productos de supermercados y tiendas.",
-        "precio": 260000
+        "precio": 260000,
+        "imagen": "dashboard.png"
     },
     {
         "id": 3,
         "nombre": "AdminData",
         "descripcion": "Aplicación con proposito de análisis de datos y graficación de datos.",
-        "precio": 250000
+        "precio": 250000,
+        "imagen": "dashboard.png"
     },
     {
         "id": 4,
         "nombre": "SuperMarkelo",
         "descripcion": "Aplicación para gestión de productos de supermercados y tiendas.",
-        "precio": 150000
+        "precio": 150000,
+        "imagen": "dashboard.png"
     },
     {
         "id": 5,
         "nombre": "AgendaBarber",
         "descripcion": "Software para gestión de agendamientos de citas para una barbería o peluquería",
-        "precio": 350000
+        "precio": 350000,
+        "imagen": "dashboard.png"
     },
     {
         "id": 6,
         "nombre": "HairCuftware",
         "descripcion": "Software para gestión de agendamientos de citas para una barbería o peluquería",
-        "precio": 50000
+        "precio": 50000,
+        "imagen": "dashboard.png"
     }
 ]
 llenarServicios();
 
 function llenarServicios(data = null) {
-    if(localStorage.getItem("servicios") == null || localStorage.getItem("servicios").length == 0){
-        localStorage.setItem("servicios",JSON.stringify( servicios));
+    if (localStorage.getItem("servicios") == null || localStorage.getItem("servicios").length == 0) {
+        localStorage.setItem("servicios", JSON.stringify(servicios));
     }
     let serviciosLocalStorage;
     if (data == null) {
-    serviciosLocalStorage = JSON.parse(localStorage.getItem("servicios"));
-    }else{
+        serviciosLocalStorage = JSON.parse(localStorage.getItem("servicios"));
+    } else {
         serviciosLocalStorage = data;
     }
     rowServiciosDOM = document.getElementById("rowServicios");
@@ -55,22 +61,22 @@ function llenarServicios(data = null) {
         console.log(`ID: ${servicio.id}, Nombre: ${servicio.nombre}, Descripción: ${servicio.descripcion}, Precio: ${servicio.precio}`);
         console.log(rowServiciosDOM);
         rowServiciosDOM.innerHTML +=
-            `<div class="col-md-3 ${((index) % 3 == 0 ) ? "" : "offset-md-1"} bg-white p-3 border-0094c6 mt-3 card-servicio">`+
-            `<div class="col-md-12 mb-2 border-0094c6">`+
-            `<img src="../Assets/Images/dashboard.png" alt="" srcset="" width="256" height="256">`+
-            `</div>`+
-            `<center>`+
-            `<h3 class="text-0094c6">$${new Intl.NumberFormat().format(servicio.precio)}</h3>`+
-            `<p>${servicio.descripcion}</p>`+
-            `<h4>${servicio.nombre}</h4>`+
-            `</center>`+
-            `</div>`+
-            `</div>`+
+            `<div class="col-md-3 ${((index) % 3 == 0) ? "" : "offset-md-1"} bg-white p-3 border-0094c6 mt-3 card-servicio">` +
+            `<div class="col-md-12 mb-2 border-0094c6">` +
+            `<img src="../Assets/Images/servicios/${servicio.imagen}" alt="" srcset="" width="256" height="256">` +
+            `</div>` +
+            `<center>` +
+            `<h3 class="text-0094c6">$${new Intl.NumberFormat().format(servicio.precio)}</h3>` +
+            `<p title="${servicio.descripcion}">${servicio.descripcion.substring(0, 75)}...</p>` +
+            `<h4 title="${servicio.nombre}">${servicio.nombre.substring(0, 20)}${(servicio.nombre.length > 20) ? "...": ""}</h4>` +
+            `</center>` +
+            `</div>` +
+            `</div>` +
             `</div>`;
     });
 }
 
-function ordenarDatosAlfabeticamente(){
+function ordenarDatosAlfabeticamente() {
     let ordenarPor = document.getElementById("ordenarPor").value;
     if (ordenarPor == 0) {
         llenarServicios();
@@ -94,4 +100,46 @@ function ordenarPorNombre(ascendente = true) {
         }
         return ascendente ? comparacion : comparacion * -1;
     });
+}
+
+/**AGREGAR SERVICIO */
+
+function agregarServicio() {
+    if (localStorage.getItem("servicios") == null || localStorage.getItem("servicios").length == 0) {
+        localStorage.setItem("servicios", JSON.stringify(servicios));
+    }
+    let nombre = document.getElementById("nombre").value;
+    let precio = document.getElementById("precio").value;
+    let descripcion = document.getElementById("descripcion").value;
+    if(!(validarCampos(nombre, precio, descripcion))) return;
+    let servicios = JSON.parse(localStorage.getItem("servicios"));
+    servicios.push({
+        "nombre": nombre,
+        "precio": parseFloat(precio),
+        "descripcion": descripcion,
+        "imagen": "dashboard.png"
+    });
+    localStorage.setItem("servicios", JSON.stringify(servicios));
+    alert(`Se ha agregado el producto ${nombre}`);
+    document.location.href="servicios.html";
+}
+
+function validarCampos(nombre, precio, descripcion){
+    let valid = true;
+    let mensajeError = "";
+    if(nombre.length < 3){
+        valid = false;
+        mensajeError += "- La longitud del nombre debe ser de mínimo 3 caracteres. \r\n";
+    }
+    if(precio.length == 0){
+        valid = false;
+        mensajeError += "- Debe asignar un precio al servicio \r\n";
+    }
+    if(descripcion.length < 20){
+        valid = false;
+        mensajeError += "- La descripción debe tener una longitud de al menos 10 caracteres. \r\n";
+    }
+    if(!valid)
+    alert(mensajeError);
+    return valid;
 }
