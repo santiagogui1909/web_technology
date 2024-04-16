c901a0810688882a1007f8d04366fe1b80a64b1e
santiagogui1909 <santiagogui1909@gmail.com>
2024-04-15 20:37:53 -0500

    Actualizacion archivos

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,6 @@
 
+let contenedorComentarios = document.querySelector(".comentarios");
+
 // Verifica si ya inicio sesion
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -169,6 +171,22 @@ function validarCampos(nombre, precio, descripcion){
     if(!valid)
     alert(mensajeError);
     return valid;
+}
+
+// Comentarios
+function capturarComentario(){
+    let sesionData = localStorage.getItem('loginUser');
+    let mensaje = document.getElementById('mensaje').value;
+
+    let fecha = new Date();
+
+    let cuerpo = `<div class="comentario_3">
+        <h3>${sesionData}</h3>
+        <h4>${mensaje}</h4>
+        <h5>Fecha Comentario ${fecha.getFullYear()}-${fecha.getMonth()}-${fecha.getDay()}</h5>
+    </div>`
+
+    contenedorComentarios.innerHTML += cuerpo;
 }
 
 // Cerrar Sesion
